4fcc79314072bf3406a7090936cc7b4ee5e10b99
Elbert Kim <elbert.kim@icloud.com>
2023-03-18 15:08:56 -0400

    test again:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import WeddingSeatingArrangement from "./tables";
-import groups from "./api/data.json"
-import { table } from "console";
+import groups from "./api/data.json";
 
 export default function LandingPage(): JSX.Element {
   return (
@@ -12,21 +10,50 @@ export default function LandingPage(): JSX.Element {
         <meta name="description" content="Landing page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen flex flex-row overflow-x-scroll">
+      <div className="min-h-screen flex flex-row overflow-x-auto scrollable-container">
         <div className="flex-shrink-0">
           <h1 className="text-center text-3xl font-bold">Table Arrangements</h1>
-          <div className="text-center text-lg italic">Feel free to sit anywhere within your table!</div>
+          <div className="text-center text-lg italic">
+            Feel free to sit anywhere within your table!
+          </div>
         </div>
         <div className="flex-nowrap flex-row flex-grow">
-          <WeddingSeatingArrangement tableName="Table 1" tableSeats={groups["Table 1"]}/>
-          <WeddingSeatingArrangement tableName="Table 2" tableSeats={groups["Table 2"]}/>
-          <WeddingSeatingArrangement tableName="Table 3" tableSeats={groups["Table 3"]}/>
-          <WeddingSeatingArrangement tableName="Table 4" tableSeats={groups["Table 4"]}/>
-          <WeddingSeatingArrangement tableName="Table 5" tableSeats={groups["Table 5"]}/>
-          <WeddingSeatingArrangement tableName="Table 6" tableSeats={groups["Table 6"]}/>
-          <WeddingSeatingArrangement tableName="Table 7" tableSeats={groups["Table 7"]}/>
-          <WeddingSeatingArrangement tableName="Table 8" tableSeats={groups["Table 8"]}/>
-          <WeddingSeatingArrangement tableName="Table 9" tableSeats={groups["Table 9"]}/>
+          <WeddingSeatingArrangement
+            tableName="Table 1"
+            tableSeats={groups["Table 1"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 2"
+            tableSeats={groups["Table 2"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 3"
+            tableSeats={groups["Table 3"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 4"
+            tableSeats={groups["Table 4"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 5"
+            tableSeats={groups["Table 5"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 6"
+            tableSeats={groups["Table 6"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 7"
+            tableSeats={groups["Table 7"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 8"
+            tableSeats={groups["Table 8"]}
+          />
+          <WeddingSeatingArrangement
+            tableName="Table 9"
+            tableSeats={groups["Table 9"]}
+          />
         </div>
       </div>
     </>
