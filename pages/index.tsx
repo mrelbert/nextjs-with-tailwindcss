61f47b5229aa30438d60b1e81f29a999b7961177
Elbert Kim <elbert.kim@icloud.com>
2023-03-18 14:47:05 -0400

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,36 @@
 import Head from "next/head";
 import Link from "next/link";
+import WeddingSeatingArrangement from "./tables";
+import Groups from "./api/data.json";
 
 export default function LandingPage(): JSX.Element {
+  const groups: {[key:string]: string[]} = Groups;
   return (
     <>
       <Head>
-        <title>Raven</title>
+        <title>N+E Seating Arrangements</title>
         <meta name="description" content="Landing page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <div className="p-8 flex flex-col justify-center items-center">
-          <div className="w-4/6 text-center">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">Raven</h1>
-            <p className="text-gray-700 leading-relaxed mb-8">
-              This app was built to organize websites that are getting scraped for Notion embeddings.
-            </p>
-          </div>
-
-          <div className="w-2/6 flex flex-wrap justify-around">
-            <Link href="/greatday" className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full mr-4 mb-4">
-              Greatday.com
-            </Link>
-            <Link href="/naver" className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full mr-4 mb-4">
-              Naver.com
-            </Link>
-            <Link href="/request" className="bg-blue-500 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full mr-4 mb-4">
-              Request
-            </Link>
-          </div>
+      <div className="container mx-auto px-4 pt-12">
+        <h1 className="text-center text-4xl font-bold text-gray-800 mb-2">
+          Table Arrangements
+        </h1>
+        <div className="text-center text-lg italic text-gray-500 mb-6 pt-2">
+          Feel free to sit anywhere within your table!
         </div>
+        <hr />  
+        <div className="pt-8 grid grid-cols-5 gap-6">
+          {Object.keys(groups).map((table, index) => (
+            <div key={index}>
+              <WeddingSeatingArrangement
+                tableName={table}
+                tableSeats={groups[table]}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
---
+++ pages/tables.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function WeddingSeatingArrangement({ tableName, tableSeats }: { tableName: string, tableSeats: string[] }) {
+  return (
+    <div>
+      <h1><b>{tableName}</b></h1>
+      <ul className='py-4'>
+        {tableSeats.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default WeddingSeatingArrangement;
